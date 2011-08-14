904e91e2fcf5cb724eee6622186ef3561d38f3cc
trademark <trademark@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-14 17:10:07 +0000

    When a value cannot be traduce to a valid one.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/SOC/2011/checks@73746 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/checks/traduction_exception.hpp
@@ -0,0 +1,20 @@
+//  Boost checks/modulus97.hpp header file  ------------------------------------//
+//  (C) Copyright Pierre Talbot 2011
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+//  See http://www.boost.org for updates, documentation, and revision history.
+
+#ifndef BOOST_CHECKS_TRADUCTION_EXCEPTION_HPP
+#define BOOST_CHECKS_TRADUCTION_EXCEPTION_HPP
+
+namespace boost{
+  namespace checks{
+
+  class traduction_exception : std::exception
+  {
+  }
+  
+}}
+
+#endif // BOOST_CHECKS_TRADUCTION_EXCEPTION_HPP
