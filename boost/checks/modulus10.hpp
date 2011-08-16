77c97518689326cc8bc923d684a5e4b8c3762d99
trademark <trademark@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-16 09:41:39 +0000

    Modified the structure algorithm and inherited from weighted_sum_algorithm.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/SOC/2011/checks@73811 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/checks/modulus10.hpp
+++ boost/checks/modulus10.hpp
@@ -12,34 +12,14 @@
 #include <boost/checks/traduction_exception.hpp>
 #include <boost/checks/weight.hpp>
 #include <boost/checks/iteration_sense.hpp>
-#include <boost/checks/basic_checks.hpp>
+#include <boost/checks/weighted_sum.hpp>
 
 namespace boost{
   namespace checks{
 
 template <typename mod10_weight, typename iteration_sense, unsigned int number_of_virtual_value_skipped = 0>
-struct modulus10_algorithm
+struct modulus10_algorithm : boost::checks::weighted_sum_algorithm<mod10_weight, iteration_sense, number_of_virtual_value_skipped>
 {
-  typedef iteration_sense iteration_sense ;
-
-  template <typename value>
-  static int traduce_to_valid_value(const value &current_value, const unsigned int valid_value_counter )
-  {
-    int valid_value = 0;
-    try{
-      valid_value = boost::lexical_cast<int>( current_value ) ;
-    }catch( boost::bad_lexical_cast ){
-      throw boost::checks::traduction_exception() ;
-    }
-    return valid_value ;
-  }
-
-  static void operate_on_valid_value( const int current_valid_value, const unsigned int valid_value_counter, int &checksum )
-  {
-    int current_weight = mod10_weight::weight_associated_with_pos( valid_value_counter + number_of_virtual_value_skipped ) ;
-    checksum += current_valid_value * current_weight ;
-  }
-
   static bool validate_checksum(int checksum)
   {
     return !(checksum % 10) ;
@@ -54,12 +34,6 @@ struct modulus10_algorithm
       throw boost::checks::traduction_exception() ;
     }
   }
-
-  template <typename check_range>
-  struct checkdigit
-  {
-    typedef typename boost::range_value<check_range>::type type;
-  };
 };
 
 }}
