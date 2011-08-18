184e68a5e01655baccb2d7e5ee2feb1c85c91e1d
trademark <trademark@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-18 22:19:54 +0000

    Add documentation.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/SOC/2011/checks@73902 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/checks/iteration_sense.hpp
+++ boost/checks/iteration_sense.hpp
@@ -5,6 +5,11 @@
 //  http://www.boost.org/LICENSE_1_0.txt
 //  See http://www.boost.org for updates, documentation, and revision history.
 
+/*! \file iteration_sense.hpp
+    \brief Provides two sense of iteration to run through the sequence from right to left or left to right.
+*/
+
+
 #ifndef BOOST_CHECK_ITERATION_SENSE_HPP
 #define BOOST_CHECK_ITERATION_SENSE_HPP
 
@@ -21,32 +26,72 @@
 namespace boost{
   namespace checks{
 
+/*! \class rightmost
+    \brief Policy class that provides methods to run through a sequence from right to left.
+*/
 struct rightmost
 {
+  /*! \class iterator
+      \brief Template rebinding class used to define the type of a const reverse iterator for seq_range.
+
+      \tparam seq_range The type of the sequence to check.
+  */
   template <typename seq_range>
   struct iterator
   {
     typedef typename boost::range_const_reverse_iterator<seq_range>::type type ;
   };
 
+  /*! \fn static typename iterator<seq_range>::type begin(seq_range &sequence)
+      \brief Get the beginning of the sequence.
+
+      \tparam seq_range The type of the sequence to check.
+      \returns A reverse iterator represents the beginning of the sequence.
+  */
   template <typename seq_range>
   static typename iterator<seq_range>::type begin(seq_range &sequence) { return boost::rbegin(sequence) ; }
 
+  /*! \fn static typename iterator<seq_range>::type end(seq_range &sequence)
+      \brief Get the ending of the sequence.
+
+      \tparam seq_range The type of the sequence to check.
+      \returns A reverse iterator represents one pass the end of the sequence.
+  */
   template <typename seq_range>
   static typename iterator<seq_range>::type end(seq_range &sequence)   { return boost::rend(sequence) ; }
 };
 
+/*! \class leftmost
+    \brief Policy class that provides methods to run through a sequence from left to right.
+*/
 struct leftmost
 {
+  /*! \class iterator
+      \brief Template rebinding class used to define the type of a const iterator for seq_range.
+
+      \tparam seq_range The type of the sequence to check.
+  */
   template <typename seq_range>
   struct iterator
   {
     typedef typename boost::range_const_iterator<seq_range>::type type ;
   };
 
+  /*! \fn static typename iterator<seq_range>::type begin(seq_range &sequence)
+      \brief Get the beginning of the sequence.
+
+      \tparam seq_range The type of the sequence to check.
+      \returns An iterator represents the beginning of the sequence.
+  */
   template <typename seq_range>
   static typename iterator<seq_range>::type begin(seq_range &sequence) { return boost::begin(sequence) ; }
+  
+  /*! \fn static typename iterator<seq_range>::type end(seq_range &sequence)
+      \brief Get the ending of the sequence.
 
+      \tparam seq_range The type of the sequence to check.
+      \returns An iterator represents one pass the end of the sequence.
+  */
   template <typename seq_range>
   static typename iterator<seq_range>::type end(seq_range &sequence)   { return boost::end(sequence) ; }
 };
