a542afeb310af8e7352879c25d4a05c7ed816c69
trademark <trademark@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-18 12:26:49 +0000

    Add check and compute function of the VISA check digit verification.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/SOC/2011/checks@73885 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/checks/visa.hpp
+++ boost/checks/visa.hpp
@@ -5,16 +5,45 @@
 //  http://www.boost.org/LICENSE_1_0.txt
 //  See http://www.boost.org for updates, documentation, and revision history.
 
-#ifndef BOOST_VISA_INCLUDED
-#define BOOST_VISA_INCLUDED
+#ifndef BOOST_CHECKS_VISA_HPP
+#define BOOST_CHECKS_VISA_HPP
 
+#include <boost/checks/luhn.hpp>
+
+#define VISA_SIZE 16
+#define VISA_SIZE_WITHOUT_CHECKDIGIT 15
 
 namespace boost {
     namespace checks{
 
-/* visa : use luhn algorithm */
 
-} // namespace checks
-} // namespace boost
+template <unsigned int number_of_virtual_value_skipped = 0>
+struct visa_algorithm : boost::checks::luhn_algorithm < number_of_virtual_value_skipped >
+{
+  static void filter_valid_value_with_pos(const unsigned int current_valid_value, const unsigned int current_value_position )
+  {
+    const unsigned int real_pos_from_left = VISA_SIZE - current_value_position - number_of_virtual_value_skipped ;
 
-#endif
+    if( real_pos_from_left == 1 && current_valid_value != 4)
+      throw std::invalid_argument("The Major Industry Identifier of a VISA credit card should be 4.") ;
+  } 
+};
+
+typedef visa_algorithm<0> visa_check_algorithm ;
+typedef visa_algorithm<1> visa_compute_algorithm ;
+
+template <typename check_range>
+bool check_visa (const check_range& check_seq)
+{
+  return boost::checks::check_sequence<visa_check_algorithm, VISA_SIZE> ( check_seq ) ;
+}
+
+template <typename check_range>
+typename boost::checks::visa_compute_algorithm::checkdigit<check_range>::type compute_visa (const check_range& check_seq)
+{
+  return boost::checks::compute_checkdigit<visa_compute_algorithm, VISA_SIZE_WITHOUT_CHECKDIGIT> ( check_seq ) ;
+}
+
+
+}}
+#endif // BOOST_CHECKS_VISA_HPP
